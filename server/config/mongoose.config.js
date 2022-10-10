a597f26d5cc0f3730c8690f909b7927d5bd3f152
David Q Rico <davidqrico@gmail.com>
2022-10-09 19:10:49 -0700

    login_auth backend added

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -6,7 +6,7 @@ const dbName = "coursesDB"
 
 
 
-mongoose.connect("mongodb+srv://davidqrico:rCnHvI3C2vB1FKFZ@cluster0.mvzfnev.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://davidqrico:@cluster0.mvzfnev.mongodb.net/?retryWrites=true&w=majority")
 
     .then(()=>{
         console.log(`Connected to the database called ${dbName}`)
